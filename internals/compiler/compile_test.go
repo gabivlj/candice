73750483869e19ab9dc79d96b8dfc0854b6a3923
Gabriel Villalonga Simón <gabitriqui@gmail.com>
2021-12-08 21:01:03 +0100

    [develop] Precedences

--- internals/compiler/compile_test.go
+++ internals/compiler/compile_test.go
@@ -11,40 +11,40 @@ import (
 
 func TestCompiler_CompileExpression_With_AddSubtractMultiplyDivide(t *testing.T) {
 	c := New()
-	c.Compile(
-		&ast.Program{
-			Statements: []ast.Statement{
-				&ast.ExpressionStatement{
-					Expression: &ast.BuiltinCall{
-						Name: "println",
-						Parameters: []ast.Expression{
-							&ast.BinaryOperation{
-								Operation: ops.Multiply,
+	root := &ast.Program{
+		Statements: []ast.Statement{
+			&ast.ExpressionStatement{
+				Expression: &ast.BuiltinCall{
+					Name: "println",
+					Parameters: []ast.Expression{
+						&ast.BinaryOperation{
+							Operation: ops.Multiply,
+							Left: &ast.BinaryOperation{
 								Left: &ast.BinaryOperation{
-									Left: &ast.BinaryOperation{
-										Operation: ops.Plus,
-										Left:      &ast.Integer{Value: 3},
-										Right:     &ast.Integer{Value: 3},
-									},
-									Right: &ast.BinaryOperation{
-										Operation: ops.Divide,
-										Left: &ast.BinaryOperation{
-											Operation: ops.Minus,
-											Left:      &ast.Integer{Value: 332},
-											Right:     &ast.Integer{Value: 1},
-										},
-										Right: &ast.Integer{Value: 3},
-									},
+									Operation: ops.Plus,
+									Left:      &ast.Integer{Value: 3},
+									Right:     &ast.Integer{Value: 3},
 								},
-								Right: &ast.Integer{Value: 5},
+								Right: &ast.BinaryOperation{
+									Operation: ops.Divide,
+									Left: &ast.BinaryOperation{
+										Operation: ops.Minus,
+										Left:      &ast.Integer{Value: 332},
+										Right:     &ast.Integer{Value: 1},
+									},
+									Right: &ast.Integer{Value: 3},
+								},
 							},
+							Right: &ast.Integer{Value: 5},
 						},
 					},
 				},
 			},
 		},
+	}
+	c.Compile(
+		root,
 	)
-
 	a.AssertEqual("580", string(a.UnwrapBytes(c.Execute())))
 }
 
--- internals/ops/operations.go
+++ internals/ops/operations.go
@@ -21,30 +21,88 @@ const (
 	GreaterThanEqual
 	LessThanEqual
 	Equals
+	NotEquals
 	Assigns
+
+	// Dot is useful for struct access
+	// like struct.thing
 	Dot
+
+	// Paren Is not going to be used for
+	// ast.BinaryOperation but is useful to retrieve the
+	// precedence of a parenthesis or function call.
+	Paren
 )
+
+var precedences = map[Operation]int{
+	AND:              1,
+	OR:               1,
+	GreaterThan:      2,
+	LessThan:         2,
+	LessThanEqual:    2,
+	GreaterThanEqual: 2,
+	Equals:           2,
+	NotEquals:        2,
+	Plus:             3,
+	Minus:            3,
+	Multiply:         4,
+	Divide:           4,
+	BinaryXOR:        5,
+	BinaryAND:        5,
+	BinaryOR:         5,
+	Dot:              6,
+	Paren:            6,
+}
+
+// Precedence retrieves the precedence of a binary operation, if the precedence
+// of the specified operation doesn't apply (maybe your Operation is Bang '!')
+// it will return 0
+func (o Operation) Precedence() int {
+	return precedences[o]
+}
 
 func (o Operation) String() string {
 	switch o {
-	case Plus: return "+"
-	case Minus: return "-"
-	case Multiply: return "*"
-	case Divide: return "/"
-	case AND: return "&&"
-	case OR: return "||"
-	case Bang: return "!"
-	case BinaryXOR: return "^"
-	case BinaryAND: return "&"
-	case BinaryOR: return "|"
-	case Reference: return "&"
-	case GreaterThan: return ">"
-	case GreaterThanEqual: return ">="
-	case LessThan: return "<"
-	case LessThanEqual: return "<="
-	case Equals: return "=="
-	case Assigns: return "="
-	case Dot: return "."
+	case Plus:
+		return "+"
+	case Minus:
+		return "-"
+	case Multiply:
+		return "*"
+	case Divide:
+		return "/"
+	case AND:
+		return "&&"
+	case OR:
+		return "||"
+	case Bang:
+		return "!"
+	case BinaryXOR:
+		return "^"
+	case BinaryAND:
+		return "&"
+	case BinaryOR:
+		return "|"
+	case Reference:
+		return "&"
+	case GreaterThan:
+		return ">"
+	case GreaterThanEqual:
+		return ">="
+	case LessThan:
+		return "<"
+	case LessThanEqual:
+		return "<="
+	case Equals:
+		return "=="
+	case Assigns:
+		return "="
+	case NotEquals:
+		return "!="
+	case Paren:
+		return "("
+	case Dot:
+		return "."
 	}
 
 	panic("unknown operand: " + strconv.FormatInt(int64(o), 10))
