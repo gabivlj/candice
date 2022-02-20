a1fe369ecdce3b662044a605d13d623427f5a5d0
Gabriel Villalonga Simón <gabitriqui@gmail.com>
2022-02-20 15:41:12 +0100

    add a recover just in case there is an internal panic

--- internals/build/builder.go
+++ internals/build/builder.go
@@ -21,6 +21,13 @@ type Project struct {
 }
 
 func ExecuteProject() {
+	defer func() {
+		output := recover()
+		if output != nil {
+			logger.Error("Internal Compiler Panic", "There has been an internal panic on the compiler", "\n", output)
+		}
+	}()
+
 	current := time.Now()
 	flags, err := retrieveFlags()
 	if err != nil {
